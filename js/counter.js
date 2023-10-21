6181f815c8f650eb2031283043f57c91eb7ed224
Leonid Dyachenko <horvitz@protonmail.ch>
2023-10-22 02:54:16 +0300

    edit

---
+++ js/counter.js
@@ -0,0 +1,114 @@
+// Хуйня 1
+let counter1 = document.getElementById("counter1");
+let counter1_minus = document.getElementById("minus-counter1");
+let counter1_plus = document.getElementById("plus-counter1");
+
+counter1_minus.addEventListener("click", function(){
+    let before = parseInt(counter1.innerHTML, 10);
+    if (before == 0){
+        alert('Вы не можете выбрать меньше 0');
+    } else {
+    counter1.innerHTML = before - 1;
+    }
+});
+
+counter1_plus.addEventListener("click", function(){
+    let before = parseInt(counter1.innerHTML, 10);
+    counter1.innerHTML = before + 1;
+}); 
+
+// Хуйня 2
+let counter2 = document.getElementById("counter2");
+let counter2_minus = document.getElementById("minus-counter2");
+let counter2_plus = document.getElementById("plus-counter2");
+
+counter2_minus.addEventListener("click", function(){
+    let before = parseInt(counter2.innerHTML, 10);
+    if (before == 0){
+        alert('Вы не можете выбрать меньше 0');
+    } else {
+    counter2.innerHTML = before - 1;
+    }
+});
+
+counter2_plus.addEventListener("click", function(){
+    let before = parseInt(counter2.innerHTML, 10);
+    counter2.innerHTML = before + 1;
+}); 
+
+// Хуйня 3
+let counter3 = document.getElementById("counter3");
+let counter3_minus = document.getElementById("minus-counter3");
+let counter3_plus = document.getElementById("plus-counter3");
+
+counter3_minus.addEventListener("click", function(){
+    let before = parseInt(counter3.innerHTML, 10);
+    if (before == 0){
+        alert('Вы не можете выбрать меньше 0');
+    } else {
+    counter3.innerHTML = before - 1;
+    }
+});
+
+counter3_plus.addEventListener("click", function(){
+    let before = parseInt(counter3.innerHTML, 10);
+    counter3.innerHTML = before + 1;
+}); 
+
+// Хуйня 4
+let counter4 = document.getElementById("counter4");
+let counter4_minus = document.getElementById("minus-counter4");
+let counter4_plus = document.getElementById("plus-counter4");
+
+counter4_minus.addEventListener("click", function(){
+    let before = parseInt(counter4.innerHTML, 10);
+    if (before == 0){
+        alert('Вы не можете выбрать меньше 0');
+    } else {
+    counter4.innerHTML = before - 1;
+    }
+});
+
+counter4_plus.addEventListener("click", function(){
+    let before = parseInt(counter4.innerHTML, 10);
+    counter4.innerHTML = before + 1;
+}); 
+
+// Хуйня 5
+let counter5 = document.getElementById("counter5");
+let counter5_minus = document.getElementById("minus-counter5");
+let counter5_plus = document.getElementById("plus-counter5");
+
+counter5_minus.addEventListener("click", function(){
+    let before = parseInt(counter5.innerHTML, 10);
+    if (before == 0){
+        alert('Вы не можете выбрать меньше 0');
+    } else {
+    counter5.innerHTML = before - 1;
+    }
+});
+
+counter5_plus.addEventListener("click", function(){
+    let before = parseInt(counter5.innerHTML, 10);
+    counter5.innerHTML = before + 1;
+}); 
+
+// Хуйня 6
+let counter6 = document.getElementById("counter6");
+let counter6_minus = document.getElementById("minus-counter6");
+let counter6_plus = document.getElementById("plus-counter6");
+
+counter6_minus.addEventListener("click", function(){
+    let before = parseInt(counter6.innerHTML, 10);
+    if (before == 0){
+        alert('Вы не можете выбрать меньше 0');
+    } else {
+    counter6.innerHTML = before - 1;
+    }
+});
+
+counter6_plus.addEventListener("click", function(){
+    let before = parseInt(counter6.innerHTML, 10);
+    counter6.innerHTML = before + 1;
+}); 
+
